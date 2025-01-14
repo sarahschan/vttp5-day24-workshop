6552f3449f1c310011168b6e969c16939f6bad51
sarahschan <sarahschan@hotmail.com>
2025-01-14 14:55:02 +0800

    first commit

---
+++ src/main/java/sg/edu/nus/iss/paf_day24_wsA/PafDay24WsAApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.paf_day24_wsA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PafDay24WsAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PafDay24WsAApplication.class, args);
+	}
+
+}
